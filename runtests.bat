f1113ebb1effbda6241132930d6b875b43f766d9
Lisa Kemper <lisa.kemper@homeinsteadinc.com>
2015-07-24 11:11:39 -0500

    fix name on nunit runners

--- runtests.bat
+++ runtests.bat
@@ -1,1 +1,1 @@
-.\packages\NUnit.Runners.Net.2.6.4\tools\nunit-console.exe .\RMWebAPI.Tests\bin\Debug\RMWebAPI.Tests.dll /noshadow
+.\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe .\RMWebAPI.Tests\bin\Debug\RMWebAPI.Tests.dll /noshadow
