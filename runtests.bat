ccdc90654163d15f95ae2b115308a2521ce80ab2
Lisa Kemper <lisa.kemper@homeinsteadinc.com>
2015-07-24 11:07:49 -0500

    fix version on nunit runners

--- runtests.bat
+++ runtests.bat
@@ -1,1 +1,1 @@
-.\packages\NUnit.Runners.Net.4.2.6.4\tools\nunit-console.exe .\RMWebAPI.Tests\bin\Debug\RMWebAPI.Tests.dll /noshadow
+.\packages\NUnit.Runners.Net.2.6.4\tools\nunit-console.exe .\RMWebAPI.Tests\bin\Debug\RMWebAPI.Tests.dll /noshadow
